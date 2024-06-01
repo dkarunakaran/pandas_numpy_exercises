ad1ab01adf1f1d4adbdc35d29804e5c1a8a334dd
Beastan <dhanoop.karunakaran@gmail.com>
2024-06-01 17:31:20 +1000

    change sin run,bash

--- run.bash
+++ run.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Run below step one by one in terminal
-
 docker build -t pandas_numpy_exercises .
-
-docker run -it -p 8888:8888 -v /home/beastan/Documents/projects/pandas_numpy_exercises:/home/jovyan/work pandas_numpy_exercises
+docker run -it -p 8888:8888 pandas_numpy_exercises
 
